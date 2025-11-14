97c091da464dbbf32ae39284b7f412a741ed9dc3
Aryan Khurana <aryankhurana1511@gmail.com>
2025-11-14 03:34:10 -0500

    chore: update DB URL

--- src/common/config.py
+++ src/common/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class AppSettings(BaseSettings):
     LOG_LEVEL: str = "INFO"
     PYTHON_ENV: str = "PROD"
-    DATABASE_URL: str
+    POSTGRES_URL: str
     SUPABASE_PUBLISHABLE_KEY: str
     SUPABASE_PROJECT_URL: str
     SUPABASE_PROJECT_REF: str
